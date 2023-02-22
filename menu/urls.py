52e8fea6a0e384f9d68513611dca4043459819f7
Mikalai Mikhailouski <jabber_mcn@tut.by>
2023-02-22 19:44:30 +0300

    initial 2

--- menu/urls.py
+++ menu/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import IndexPageView, Index1PageView
+from .views import IndexPageView
 
 app_name = 'menu'
 
 urlpatterns = [
     path('menu/', IndexPageView.as_view(), name='index'),
-    path('menu1/', Index1PageView.as_view(), name='index')
 ]
--- menu/views.py
+++ menu/views.py
@@ -3,7 +3,3 @@ from django.views.generic import TemplateView
 
 class IndexPageView(TemplateView):
     template_name = "menu/index.html"
-
-
-class Index1PageView(TemplateView):
-    template_name = "menu/index1.html"
